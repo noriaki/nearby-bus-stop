803fea048f17fd55d5ef08aa2942433840c361c1
Noriaki Uchiyama <uchiyama.noriaki@gmail.com>
2017-08-14 23:00:07 +0900

    add bus-stop data sources

---
+++ batch/sources.js
@@ -0,0 +1,33 @@
+module.exports = [
+  // *: No timetable
+  // **: No timetable when saturday, holiday
+  // ホテルマリナーズコート東京前
+  { stopId: 1251, poleNo: 1, routeId: 23 }, //  都03 四谷駅行
+  { stopId: 1251, poleNo: 1, routeId: 46 }, //  錦13 錦糸町駅前行
+  { stopId: 1251, poleNo: 1, routeId: 47 }, //  錦13出入 深川車庫前行*
+  { stopId: 1251, poleNo: 1, routeId: 184 }, // 都05-1 東京駅丸の内南口行
+  { stopId: 1251, poleNo: 2, routeId: 23 }, //  都03 晴海埠頭行
+  { stopId: 1251, poleNo: 2, routeId: 46 }, //  錦13 晴海埠頭行
+  { stopId: 1251, poleNo: 2, routeId: 183 }, // 都05-1出入 晴海埠頭行*
+  { stopId: 1251, poleNo: 2, routeId: 184 }, // 都05-1 晴海埠頭行
+  // 晴海三丁目
+  { stopId: 1253, poleNo: 1, routeId: 46 }, //  錦13 錦糸町駅前行
+  { stopId: 1253, poleNo: 1, routeId: 47 }, //  錦13出入 深川車庫前行*
+  { stopId: 1253, poleNo: 1, routeId: 49 }, //  東15 深川車庫前行
+  { stopId: 1253, poleNo: 1, routeId: 183 }, // 都05-1出入 深川車庫前行
+  { stopId: 1253, poleNo: 2, routeId: 23 }, //  都03 四谷駅行
+  { stopId: 1253, poleNo: 2, routeId: 49 }, //  東15 東京駅八重洲口行
+  { stopId: 1253, poleNo: 2, routeId: 179 }, // 深夜13 東京駅丸の内南口行**
+  { stopId: 1253, poleNo: 2, routeId: 181 }, // 都05-2 東京駅丸の内南口行
+  { stopId: 1253, poleNo: 2, routeId: 182 }, // 都05-2急行 東京駅丸の内南口行**
+  { stopId: 1253, poleNo: 2, routeId: 183 }, // 都05-1出入 東京駅丸の内南口行*
+  { stopId: 1253, poleNo: 2, routeId: 184 }, // 都05-1 東京駅丸の内南口行
+  // 晴海トリトンスクエア前
+  { stopId: 2442, poleNo: 1, routeId: 23 }, //  都03 晴海埠頭行
+  { stopId: 2442, poleNo: 1, routeId: 49 }, //  東15 深川車庫前行
+  { stopId: 2442, poleNo: 1, routeId: 179 }, // 深夜13 有明一丁目行**
+  { stopId: 2442, poleNo: 1, routeId: 181 }, // 都05-2 東京ビッグサイト行
+  { stopId: 2442, poleNo: 1, routeId: 182 }, // 都05-2急行 東京ビッグサイト行**
+  { stopId: 2442, poleNo: 1, routeId: 183 }, // 都05-1出入 深川車庫前行*
+  { stopId: 2442, poleNo: 1, routeId: 184 }, // 都05-1 晴海埠頭行
+];
